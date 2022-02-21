df7f7e2c7cec5294d8b4044c0256be57f9b3a471
O365-MB <ryan.paulos@ux-1.labs.encs.vancouver.wsu.edu>
2022-02-20 19:34:16 -0800

    +Argument parsing

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
 
-void process_args(int argc, char* argv[]) {
+int valid_args(int argc, char* argv[], unsigned int *);
 void print_usage();
 
 void main(int argc, char *argv[]) {
+    printf("argc: %d\n", argc);
 
-    // Process args
-    printf("arc: %d", argc);
-    
+    unsigned int t_slice;
+    if (!valid_args(argc, argv, &t_slice)) {
+        print_usage();
+        exit(-1);
+    }
+    // Argument format valid. Checking input file
+    printf("Arguments check out (haven't checked the file yet\n");    
 
 }
 
@@ -18,36 +24,53 @@ void print_usage() {
     printf("Usage: -alg [FIFO|SJF|PR|RR] [-quantum [integer(ms)]] -input [file name]\n");
 }
 
-int valid_alg(char * alg) {
-    if (!strcmp("FIFO", alg) return 1;
-    if (!strcmp("SJF", alg) return 1;
-    if (!strcmp("PR", alg) return 1;
-    if (!strcmp("RR", alg) return 1;
+int valid_alg(char *alg) {
+    if (!strcmp("FIFO", alg)) return 1;
+    if (!strcmp("SJF", alg)) return 1;
+    if (!strcmp("PR", alg)) return 1;
+    if (!strcmp("RR", alg)) return 1;
     return 0;
 }
 
-void process_args(int argc, char* argv[]) {
-    // need to at least specify an algorithm
-    if (argc < 3) {
-        print_usage();
-    }
-    
-    // Checking first argument
-    if (strcmp(argv[1], "-arg")) {
-        print_usage();
-    }
-    if (!valid_alg(argv[2])) {
-        print_usage();
-        exit(-1);
-    } 
-
-    // Checking for quantum/input file
-    if (argc == 4) {
-        // Missing an argument value
-        print_usage();
-    }
-    // argc >= 5
+int valid_file(char *in_arg, char *file_name) {
     
 }
 
+int valid_args(int argc, char* argv[], unsigned int *t_slice) {
+    /* Evaluates the command line arguments for validity.
+     Does not attempt to open the input file 
+
+     returns: 1 if args are valid, else 0 */
+
+    if (argc != 3 && argc != 5 && argc != 7) {
+        printf("Arg count fail\n");
+        return 0;
+    }
+    // Checking first argument
+    if (strcmp(argv[1], "-alg")) {
+        printf("alg input fail\n");
+        return 0;
+    }
+    if (!valid_alg(argv[2])) {
+        return 0;
+    }   
+    // Checking for -input switch
+    if (strcmp("-input", argv[argc - 2])) {
+        printf("input switch fail\n");
+        return 0;
+    } 
+    // Evaluating -quantum switch and arg
+    if (argc == 7) {
+        if (strcmp("-quantum", argv[3])) {
+            return 0;
+        } 
+        // sscanf to get the integer here.
+        int ret = sscanf(argv[4], "%d", t_slice);
+        if (ret != 1) {
+            return 0;
+        }
+        
+    }
+    return 1;
+}
 
